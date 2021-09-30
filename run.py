bbdcc5664e78ed70528efb062bd17201e7cfbe2f
Surabhi Sharma <89366895+surabhi-sharm@users.noreply.github.com>
2021-09-29 23:48:51 -0400

    Add files via upload

---
+++ run.py
@@ -0,0 +1,20 @@
+from flask import Flask
+import time
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello world!'
+
+@app.route('/time')
+def getTime():
+    return time.strftime("%Y %m %d %H:%M:%S",time.localtime())
+
+
+
+app.run(host='0.0.0.0',
+        port=8080,
+        debug=True)
+
